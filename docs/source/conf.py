b9fe8fa0817096601d26604fbb97e47a2856ae54
Valentin Niess <niess@in2p3.fr>
2026-04-21 17:00:59 +0200

    Bump version: v1.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 project = "Calzone"
 copyright = "Université Clermont Auvergne, CNRS/IN2P3, LPCA"
 author = "Valentin Niess"
-release = "1.2.0"
+release = "1.2.1"
 
 highlight_language = "python3"
 
--- src/python/calzone/__init__.py
+++ src/python/calzone/__init__.py
@@ -11,4 +11,4 @@ def init():
 init()
 del init
 
-VERSION = "1.2.0"
+VERSION = "1.2.1"
